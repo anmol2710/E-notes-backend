f0d013986f2f8a5a4dea0935606482539e61c3ce
Anmol Garg <anmolgarg271005@gmail.com>
2024-02-11 19:29:45 +0530

    "Add api in routes"

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -24,7 +24,28 @@ router.post("/signup" , async(req,res)=>{
     password = bcrypt.hashSync(password , salt);
     user = await USER.create({name, rollNo , email , password, branch})
     if(user){
-        console.log(user)
+        return res.json({msg:user , status:true})
+    }
+    else{
+        return res.json({msg:"Internal Server Error", status:false})
+    }   
+})
+
+router.post("/api/signup" , async(req,res)=>{
+    let {name , rollNo , email , password} = req.body;
+    let user = await USER.findOne({email});
+    if(user){
+        return res.json({msg:"Email already exist" , status:false});
+    }
+
+    user = await USER.findOne({rollNo});
+    if(user){
+        return res.json({msg:"RollNo already exist" , status:false});
+    }
+    const branch = rollNo.charAt(5) + rollNo.charAt(6)
+    password = bcrypt.hashSync(password , salt);
+    user = await USER.create({name, rollNo , email , password, branch})
+    if(user){
         return res.json({msg:user._id , status:true})
     }
     else{
@@ -37,6 +58,22 @@ router.get("/login" , (req , res)=>{
 })
 
 router.post("/login" , async(req,res)=>{
+    let {rollNo , password} = req.body;
+    let user = await USER.findOne({rollNo});
+    if(user){
+        if(bcrypt.compareSync(password , user.password)){
+            return res.json({msg:user , status:true})
+        }
+        else{
+            return res.json({msg:"Invalid Password" , status:false})
+        }
+    }   
+    else{
+        return res.json({msg:"User Not exist" , status:false})
+    }
+})
+
+router.post("/api/login" , async(req,res)=>{
     let {rollNo , password} = req.body;
     let user = await USER.findOne({rollNo});
     if(user){
